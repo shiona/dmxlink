a59806df4395385fb217e50a1425618642d7fa77
Juho <juho.jokelainen@vincit.fi>
2022-12-21 01:29:10 +0200

    Add time machine effect

--- DMXreceiver/DMXreceiver.ino
+++ DMXreceiver/DMXreceiver.ino
@@ -27,7 +27,7 @@ const uint8_t B_PIN[] = {3};
 const uint8_t strip_count = 1;
 
 #define RGB_DATA_PIN 5
-#define NUM_LEDS 300
+#define NUM_LEDS 297
 CRGB leds[NUM_LEDS];
 
 void set_led(uint8_t led_pin, uint8_t brightness) {
@@ -52,6 +52,10 @@ void setup() {
   radio.startListening();
 }
 
+static const CRGB LED_OFF = CRGB(0, 0, 0);
+static const CRGB LED_RED = CRGB(255, 0, 0);
+static const CRGB LED_BLUE = CRGB(0, 0, 255);
+
 void loop() {
  // timing stuff
  #define speed 4
@@ -67,30 +71,58 @@ void loop() {
 
       done = radio.read(&data, sizeof(data));
 
-      uint8_t lit = data[0];
+      uint8_t state = data[0];
       uint8_t r = data[1];
       uint8_t g = data[2];
       uint8_t b = data[3];
 
       //uint16_t lit_led_count = ((NUM_LEDS) * (uint32_t)(lit)) / 255;
-      uint16_t lit_led_count = ((NUM_LEDS) * (uint32_t)(lit)) / 255;
 
-      for (uint16_t i = 0; i < NUM_LEDS; i++)
+      if (state == 0)
       {
-        uint8_t intensity = sine_wave_pow_2[(u - 137*i)%256];
-        //intensity = 10 + intensity >> 1;
-        if (i < lit_led_count)
+        for (uint16_t i = 0; i < NUM_LEDS; i++)
         {
-          uint8_t r2 = (r * intensity) >> 8;
-          uint8_t g2 = (g * intensity) >> 8;
-          uint8_t b2 = (b * intensity) >> 8;
-          //leds[i] = CRGB(intensity >> 7, intensity >> 5, intensity >> 3);
-          leds[NUM_LEDS - i - 1] = CRGB(r2, g2, b2);
-          //leds[i] = CRGB(200, g2, b2);
+          leds[i] = LED_OFF;
         }
-        else
+      }
+      // This case handles both "stand by" with loading state 0 (state 1-10)
+      // and the actual loading bars
+      else if (state <= 210)
+      {
+        // Loading bar led count
+        uint16_t lit_led_count = 0;
+        if (state > 10)
         {
-          leds[NUM_LEDS - i - 1] = CRGB(0, 0, 0);
+          // lit per side
+          lit_led_count = state - 10;
+        }
+
+        for (uint16_t i = 0; i < NUM_LEDS/2; i++)
+        {
+          uint8_t intensity = sine_wave_pow_2[(u - 137*i)%256];
+          //intensity = 10 + intensity >> 1;
+          if (i < lit_led_count)
+          {
+            leds[NUM_LEDS - i - 1] = LED_RED;
+            leds[i] = LED_RED;
+          }
+          else
+          {
+            uint8_t r2 = (r * intensity) >> 8;
+            uint8_t g2 = (g * intensity) >> 8;
+            uint8_t b2 = (b * intensity) >> 8;
+            //leds[i] = CRGB(intensity >> 7, intensity >> 5, intensity >> 3);
+            leds[NUM_LEDS - i - 1] = CRGB(r2, g2, b2);
+            leds[i] = CRGB(r2, g2, b2);
+            //leds[i] = CRGB(200, g2, b2);
+          }
+        }
+      }
+      else
+      {
+        for (uint16_t i = 0; i < NUM_LEDS; i++)
+        {
+          leds[i] = LED_BLUE;
         }
       }
 
